8e22b3d9b8a0eea5616c49ee07d063d3dc761dc7
dk99kodehode <dk99kodehode@gmail.com>
2026-06-30 21:48:27 +0200

    changes to edit btn and converting numbers into mins & hours

--- javascript.js
+++ javascript.js
@@ -10,23 +10,40 @@ submitExercise.addEventListener("click", () => {
   const exerciseValue = exerciseInput.value.trim();
   const durationValue = durationInput.value.trim();
 
+  /*----DIVEDE THE HOURS BY 60 AND CONVER THE LEFT OVER IN DURATION FROM DIVIDING IT BY 60 AND TURNING INTO MINTUES*/
+  const hours = Math.floor(durationValue / 60);
+  const mins = durationValue % 60;
+
   if (exerciseValue !== "" && durationValue !== "") {
     const session = document.createElement("div");
     session.classList.add("session");
 
     const exercise = document.createElement("input");
-    exercise.value = exerciseValue;
     exercise.classList.add("exercise");
     exercise.readOnly = true;
+    exercise.value = exerciseValue;
 
     const duration = document.createElement("input");
-    duration.value = `${durationValue} min(s)`;
+    duration.value = `${hours}h ${mins}m`;
     duration.classList.add("duration");
     duration.readOnly = true;
 
     const editBtn = document.createElement("button");
     editBtn.textContent = "Edit";
     editBtn.id = "edit";
+    editBtn.addEventListener("click", () => {
+      if (editBtn.textContent === "Edit") {
+        exercise.readOnly = false;
+        duration.readOnly = false;
+
+        editBtn.textContent = "Save";
+      } else {
+        exercise.readOnly = true;
+        duration.readOnly = true;
+
+        editBtn.textContent = "Edit";
+      }
+    });
 
     const favoritesBtn = document.createElement("button");
     favoritesBtn.textContent = "Add to favorites";
